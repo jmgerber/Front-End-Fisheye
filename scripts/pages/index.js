f24d3500c073d04f0f87126d33fb735a09d64181
jmgerber <jmgerber57@gmail.com>
2022-10-01 13:26:35 +0200

    Using Fetch to call datas

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,11 @@
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        const response = await fetch('../../data/photographers.json');
+            if(response.ok){
+                const photographers = await response.json();
+                return photographers
+            } else {
+                console.error(`Returned error : ${response.status}`);
+            }
     }
 
     async function displayData(photographers) {
