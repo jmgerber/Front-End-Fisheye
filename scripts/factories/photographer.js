ca17a58190796eae50bc684477d001e4ef1e209a
jmgerber <jmgerber57@gmail.com>
2022-10-01 17:38:58 +0200

    Design page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,21 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `./assets/photographers/ID_photos/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+      const article = document.createElement('article');
+      const photographerCard = `
+        <a href ="#">
+          <img src="${picture}" alt="${name}" />
+          <h2>${name}</h2>
+        </a>
+        <p class="card-city">${city}, ${country}</p>
+        <p class="card-tagline">${tagline}</p>
+        <p class="card-price">${price}€/jour</p>
+      `
+      article.innerHTML = photographerCard
+      return (article)
     }
     return { name, picture, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -21,6 +21,7 @@
     async function init() {
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
+        // Fonction d'affichage des photographes
         displayData(photographers);
     };
     
